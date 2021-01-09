0dd37f90dcacf384af8104dab703613461581ded
wook3024 <wook3024@gmail.com>
2021-01-10 08:53:36 +0900

    add make_dataset

--- server_test.py
+++ server_test.py
@@ -9,26 +9,46 @@ import random
 import struct
 
 
-# 01043, 01125, 01543, 01619, 01904, 01916, 02603, 29734, 29879, 30032, 30325, 31841, 32379
+# 01043, 01125, 01543, 01619, 01904, 01916, 32379 
+# 02603, 29734, 29879, 30032, 30325, 31841
 # ========================= infomation ========================= #
 ip_address = '192.168.154.29'
 port = '8000'
+image_extentions = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']
 root_dir = './dataset/FFHQ_AnimeFaces256Cleaner/testA/'
 image_name = '32379.png'
 target_dir = os.path.join(root_dir, image_name)
-image_set_shuffle = True
-ratio_control = False
-style_control = False
+shuffle_images = False
+control_IN_LN_ratio = False
+control_style = False
 multi_inference = True
 
 
-image_set = glob.glob(os.path.join(root_dir, '*.png'))
-if image_set_shuffle:
-    random.shuffle(image_set)
+# ========================= make dataset ========================= #
+def extension_check(file_name):
+    file_name_lower = file_name.lower()
+    possible = any(file_name_lower.endswith(image_extention) for image_extention in image_extentions)
+    return possible
+
+def make_dataset(root_dir):
+    image_dirs = []
+    for root, _, files in sorted(os.walk(root_dir)):
+        for file_name in sorted(files):
+            if extension_check(file_name):
+                image_dir = os.path.join(root, file_name)
+                image_dirs.append(image_dir)
+
+    return image_dirs
+
+image_dirs = make_dataset(root_dir)
+
+if shuffle_images:
+    random.shuffle(image_dirs)
 
 
+# ========================= start iteration ========================= #
 concat_image_set = None
-for index, image_dir in enumerate(image_set):
+for index, image_dir in enumerate(image_dirs):
     try:
         ratio_control_value += 0.1
         style_control_value += 0.1
@@ -36,22 +56,22 @@ for index, image_dir in enumerate(image_set):
         ratio_control_value = -1.0
         style_control_value = 0.0
     
-    if not ratio_control: ratio_control_value = 0.0
-    if not style_control: style_control_value = 1.0
-    # ========================= original ========================= #
-    original_image = Image.open(target_dir if ratio_control or style_control else image_dir).convert('RGB')
+    if not control_IN_LN_ratio: ratio_control_value = 0.0
+    if not control_style: style_control_value = 1.0
+    # ========================= original image ========================= #
+    original_image = Image.open(target_dir if control_IN_LN_ratio or control_style else image_dir).convert('RGB')
     original_image_arr = np.array(original_image, dtype=np.uint8)
     original_image_arr = cv2.resize(original_image_arr, dsize=(256, 256), interpolation=cv2.INTER_AREA)
 
-    # ========================= translation ========================= #
+    # ========================= translation image ========================= #
     resp = requests.post(f"http://{ip_address}:{port}/predict", files={
-        "image": open(target_dir if ratio_control or style_control else image_dir, 'rb'), 
+        "image": open(target_dir if control_IN_LN_ratio or control_style else image_dir, 'rb'), 
         "ratio_control_value": struct.pack('f', ratio_control_value), 
         "style_control_value": struct.pack('f', style_control_value)})
     translation_image_arr = np.array(resp.json()['image'], dtype=np.uint8)
     # translation_image = Image.fromarray(translation_image_arr)
 
-    # ========================= concat and show ========================= #
+    # ========================= image concat and show ========================= #
     concat_image = np.concatenate((original_image_arr, translation_image_arr), axis=0)
     if concat_image_set is not None:
         concat_image_set = np.concatenate((concat_image_set, concat_image), axis=1)
